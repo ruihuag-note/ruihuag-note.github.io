de1bdee0bc717d9a749ad80e9f7ac7115e8bcf16
grh-gitee <3494242908@qq.com>
2024-02-28 17:25:36 +0800

    docs: 2024-02-28 17:25:36

--- config/build/presave.js
+++ config/build/presave.js
@@ -1,5 +1,6 @@
 const { exec } = require('child_process')
 
+console.log('Running lv1 before')
 exec('node config/build/build-sidebar-lv1.js', (error, stdout, stderr) => {
   if (error) {
     console.error(`执行命令时出错: ${error}`)
@@ -8,6 +9,7 @@ exec('node config/build/build-sidebar-lv1.js', (error, stdout, stderr) => {
   console.log(`命令输出: ${stdout}`)
 })
 
+console.log('Running lv2 before')
 exec('node config/build/build-sidebar-lv2.js', (error, stdout, stderr) => {
   if (error) {
     console.error(`执行命令时出错: ${error}`)
@@ -15,3 +17,4 @@ exec('node config/build/build-sidebar-lv2.js', (error, stdout, stderr) => {
   }
   console.log(`命令输出: ${stdout}`)
 })
+
